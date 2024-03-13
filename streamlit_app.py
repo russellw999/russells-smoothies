8bff40956a0c3f34558da8d001a9c72faab32357
Russell W <29832103+russellw999@users.noreply.github.com>
2024-03-13 17:50:48 +0100

    missing name_on_order in insert statement
    fix missing name_on_order in insert statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,10 +30,10 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-    #st.write(ingredients_string)    
-    
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-                values ('""" + ingredients_string + """')"""
+    #st.write(ingredients_string) 
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+ name_on_order +"""')"""    
     
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
